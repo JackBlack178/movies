4a2439223c3a1fb43f2860b25cf9340b813fe314
Ilyas Safiullin <Safiullin1Ilyas.yandex.ru>
2024-09-03 03:04:46 +0300

    header created

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,45 @@
-import React, { FC } from 'react'
+import createTheme from '@mui/material/styles/createTheme'
+import { NavLink } from 'react-router-dom'
+import Button from '@mui/material/Button'
+import { ThemeProvider } from '@emotion/react'
+import { LogoIcon } from './icons/logo-icon'
+
+const buttonTheme = createTheme({
+	palette: {
+		primary: {
+			main: '#333',
+			contrastText: '#fff',
+		},
+	},
+})
 
 const Header = () => {
-	return <header>12</header>
+	return (
+		<header className=' bg-orange-300 p-4 grid grid-cols-3 justify-items-center gap-x-4 items-center'>
+			<LogoIcon className='h-auto max-w-[50px] justify-self-start' />
+
+			<nav className='flex gap-x-4'>
+				<NavLink to='/' className=' hover:text-gray-600 duration-200'>
+					Home
+				</NavLink>
+				<NavLink to='/my' className=' hover:text-gray-600 duration-200'>
+					Saved
+				</NavLink>
+				<NavLink to='/favorites' className=' hover:text-gray-600 duration-200'>
+					Favorites
+				</NavLink>
+			</nav>
+			<ThemeProvider theme={buttonTheme}>
+				<Button
+					variant='contained'
+					size='medium'
+					className='max-w-[200px] justify-self-end bg-slate-600 hover:bg-slate-500 hover:transition-all'
+				>
+					Sign in/Sign up
+				</Button>
+			</ThemeProvider>
+		</header>
+	)
 }
 
 export { Header }
